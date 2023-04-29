0c510b8030486ea056c325b180bf811c89a38afa
fouad011 <mourchidfouad22@hotmail.com>
2023-04-29 04:00:42 -0700

    update

--- withPython/classic_encryption/vigenere_cipher/vigenere_cipher.py
+++ withPython/classic_encryption/vigenere_cipher/vigenere_cipher.py
@@ -1,31 +1,33 @@
-tab1 = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ", range(0, 26)))
-tab2 = dict(zip(range(0, 26), "ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
-def codage(c):
-    return tab1[c]
-def decodage(n):
-    return tab2[n]
+def code(x):
+    if type(x) is str :
+        return ord(x)-65
+    elif type(x) is int:
+        return chr(x+65)
+
 def chiffre(M, k):
     Mn = normalise(M)
     C = ''
     i = 0
     for c in Mn:
-        n = (codage(c)+k[i])%26
-        C += decodage(n)
+        n = (code(c)+k[i])%26
+        C += code(n)
         i += 1
         if len(k)==i:
             i = 0
     return C
+
 def dechiffre(C, k):
     Cn = normalise(C)
     M = ''
     i = 0
     for c in Cn:
-        n = (codage(c)-k[i])%26
-        M += decodage(n)
+        n = (code(c)-k[i])%26
+        M += code(n)
         i += 1
         if len(k)==i:
             i = 0
     return M
+
 def normalise(m):
     m = m.upper()
     m = m.split(' ')
@@ -33,11 +35,13 @@ def normalise(m):
     for x in m:
         M += x
     return M
+
 def clef(k):
     cles = []
     for c in k:
-        cles.append(codage(normalise(c)))
+        cles.append(code(normalise(c)))
     return cles
+
 def Menu():
     print("0\t:\tencrypt")
     print("1\t:\tdecrypt")
@@ -51,4 +55,5 @@ def Menu():
         print("encrypt message : ", chiffre(msg, key))
     elif s==1:
         print("decrypt message : ", dechiffre(msg, key))
+
 Menu()
